6c9325a10427e53a615b4ffb8676e0c55d38e3c1
vemundtl <vemund@thallaug.org>
2022-02-27 18:20:43 +0100

    undergrupper page in progress

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import Home from "./screens/Home";
 import OmOss from "./screens/OmOss";
+import Undergrupper from "./screens/Undergrupper";
 
 export interface defaultProps extends RouteComponentProps {}
 
@@ -17,6 +18,11 @@ const App: React.FC = () => {
       <BrowserRouter>
         <Switch>
           <Route path="/" exact render={(props) => <Home {...props} />} />
+          <Route
+            path="/Undergrupper"
+            exact
+            render={(props) => <Undergrupper {...props} />}
+          />
           <Route path="/OmOss" exact render={(props) => <OmOss {...props} />} />
         </Switch>
       </BrowserRouter>
