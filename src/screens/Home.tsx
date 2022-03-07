c5ae17b21801bfb23365c2cfa810791fd61788ed
vemundtl <vemund@thallaug.org>
2022-03-07 12:20:45 +0100

    undergrupper finish?

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -6,10 +6,10 @@ import styrebilde from "../bilderr/styrebilde.jpg";
 import logo from "../bilderr/logo.png";
 import bottom from "../bilderr/bottom.jpg";
 import ReactPlayer from "react-player";
-import { FaBeer } from 'react-icons/fa';
-import { FaFacebook } from 'react-icons/fa';
-import { FaInstagram } from 'react-icons/fa';
-
+import { FaBeer } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import Undergruppe from "../screens/Undergrupper";
 
 import {
   createStyles,
@@ -26,56 +26,61 @@ interface Props {
   history: History<any>;
 }
 
-
 const Home: React.FC<Props> = ({ history }) => {
   const classes = useStyles();
   return (
     <div>
       {/*<AppBar onOmOss={() => history.push("/OmOss")} history={history}></AppBar>*/}
-      
+
       <div className="front">
-        <img className="header" src={logo} ></img>
-       </div>
+        <img className="header" src={logo}></img>
+      </div>
       <div className="btns">
-      <a href="https://www.youtube.com/watch?v=67ulkWeWlNM"><button className="promo" >SE PROMOVIDEOEN VÅR HER</button></a>
-      <a href="https://tikkio.com/tickets/28027-hybridarevyen-2022-bare-revy?fbclid=IwAR1310KnEqsoF-gYpNwWKt89DreCbEREfmDUZ1WORYWE1xgp-idOaDSbU-I"><button className="scroll" >KJØP BILETTER</button></a>
+        <a href="https://www.youtube.com/watch?v=67ulkWeWlNM">
+          <button className="promo">SE PROMOVIDEOEN VÅR HER</button>
+        </a>
+        <a href="https://tikkio.com/tickets/28027-hybridarevyen-2022-bare-revy?fbclid=IwAR1310KnEqsoF-gYpNwWKt89DreCbEREfmDUZ1WORYWE1xgp-idOaDSbU-I">
+          <button className="scroll">KJØP BILETTER</button>
+        </a>
       </div>
 
-
-    <div className="main_box">
-      <div className="sub_box">
-      <h1>Om oss</h1>
-      <hr/>
-      <p>
-        I år har Hybrida stått på for å få igang en revy for aller første gang!
-        På bilde ser du oss i revystyret. Men det er ikke så viktig. Det viktige
-        er at vi trenger deg for å få til en skikkelig bra revy. 
-      </p>
-      <p>
-      For selv om vi
-        brenner veldig for å få til en revy, er ikke dette noe vi kan klare
-        alene. Så hva kan du hjelpe til med i en revy spør du? Alt, absolutt
-        alt, nesten. Vi trenger hvert fall folk til både små og store verv. Er
-        det en ting vi kan garantere, så er virkelig gode muligheter til å bli
-        kjent med folk på linja! Revyen består av oss i styret, skuespillere,
-        band, PR-gruppe, kosegruppe, manusgruppe, scenegruppe. Det beste? Revyen
-        har plass til alle som vil. Enten du har mye tid for hånd og vil
-        dypdykke deg inn i revyarbeidet, eller om du kanskje ikke har så mye
-        tid, men vil hjelpe til der det trengs, null problem!
-      </p>
-       <p>  Å få til en bra
-        revy er selvfølgelig dritkult, men enda kulere er det å få til masse
-        sosialt for hybrider og skape et godt sosialt miljø for alle som vil
-        delta! Så enten du ønsker en stor skuespillerrolle, vil lage kostymer,
-        planlegge revyfester, snekre rekvisitter, spille tuba, temme slanger
-        eller være ledningssorterer så håper vi at du benytter muligheten til å
-        bli med i dette fantastiske felleskapet Hybridarevyen skal være!
-      </p>
+      <div className="main_box">
+        <div className="sub_box">
+          <h1>Om oss</h1>
+          <hr />
+          <p>
+            I år har Hybrida stått på for å få igang en revy for aller første
+            gang! På bilde ser du oss i revystyret. Men det er ikke så viktig.
+            Det viktige er at vi trenger deg for å få til en skikkelig bra revy.
+          </p>
+          <p>
+            For selv om vi brenner veldig for å få til en revy, er ikke dette
+            noe vi kan klare alene. Så hva kan du hjelpe til med i en revy spør
+            du? Alt, absolutt alt, nesten. Vi trenger hvert fall folk til både
+            små og store verv. Er det en ting vi kan garantere, så er virkelig
+            gode muligheter til å bli kjent med folk på linja! Revyen består av
+            oss i styret, skuespillere, band, PR-gruppe, kosegruppe,
+            manusgruppe, scenegruppe. Det beste? Revyen har plass til alle som
+            vil. Enten du har mye tid for hånd og vil dypdykke deg inn i
+            revyarbeidet, eller om du kanskje ikke har så mye tid, men vil
+            hjelpe til der det trengs, null problem!
+          </p>
+          <p>
+            {" "}
+            Å få til en bra revy er selvfølgelig dritkult, men enda kulere er
+            det å få til masse sosialt for hybrider og skape et godt sosialt
+            miljø for alle som vil delta! Så enten du ønsker en stor
+            skuespillerrolle, vil lage kostymer, planlegge revyfester, snekre
+            rekvisitter, spille tuba, temme slanger eller være ledningssorterer
+            så håper vi at du benytter muligheten til å bli med i dette
+            fantastiske felleskapet Hybridarevyen skal være!
+          </p>
+        </div>
+        <header className="header">Styret 2022</header>
+        <img src={styrebilde} alt="test" className="styrebilde"></img>
+        <Undergruppe />
       </div>
-      <header className="header">Styret 2022</header>
-      <img src={styrebilde} alt="test" className="styrebilde"></img>
     </div>
-  </div>
   );
 };
 
